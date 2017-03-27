651deed98d55a0752713d4a7c96253abaaa30a84
rajesh peddalachugari <rajesh.peddalachugari@gmail.com>
2017-03-27 23:25:09 +0530

    article

---
+++ js/flowtypeAdd.js
@@ -0,0 +1,21 @@
+﻿$('.ph').flowtype({
+    minimum: 300,
+    maximum: 1800,
+    minFont: 14,
+    maxFont: 36
+    //fontRatio: 30
+});
+$('.authpanel').flowtype({
+    minimum: 300,
+    maximum: 1800,
+    minFont: 12,
+    maxFont: 18
+    
+});
+$('p').flowtype({
+    minimum: 300,
+    maximum: 1800,
+    minFont: 12,
+    maxFont: 15,
+    
+});
