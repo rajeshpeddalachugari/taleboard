5c8075d6d2156c84249c371f6b93aece6e766fbf
rajesh peddalachugari <rajesh.peddalachugari@gmail.com>
2017-04-16 13:04:56 +0530

    modifications

--- js/script.js
+++ js/script.js
@@ -1,5 +1,19 @@
-﻿function myFunction() {
-    var x = document.getElementById("demo");
+﻿/// <reference path="jquery-3.2.0.min.js" />
+
+
+$(document).ready(function (){
+    $('.filtertags > a').click(function () {
+        $(this).toggleClass("w3-blue");
+        $(this).toggleClass("w3-light-gray");
+    });
+});
+
+
+
+function Accord(id) {
+    $(".toggledown").toggle();
+    $(".toggleup").toggle();
+    var x = document.getElementById(id);
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
     } else {
